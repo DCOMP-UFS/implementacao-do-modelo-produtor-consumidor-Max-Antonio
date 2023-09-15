6b4482843a85e9be2017fdf56211953c0515362c
Max Antônio <114777087+Max-Antonio@users.noreply.github.com>
2023-09-15 15:49:37 -0300

    Update parte2.c

--- parte2.c
+++ parte2.c
@@ -4,138 +4,240 @@
 #include <semaphore.h>
 #include <unistd.h>
 #include <time.h>
+#include <mpi.h> 
 
-#define SIZE 10 //tamanho máximo da fila
-#define THREAD_NUM 3 //número de pares de threads (nesse caso, 3 produtoras e 3 consumidoras)
+// * Compilação: mpicc -o parte3 parte3.c
+// * Execução:   mpiexec -n 3 ./parte3
 
-//Compile:  gcc -g -Wall -o parte2 parte2.c -lpthread -lrt
-//Usage:    ./parte2
+
+#define THREAD_NUM 1
+#define SIZE 10
 
 typedef struct Clock { 
    int p[3];
 } Clock;
 
-Clock filaClock[SIZE];
-int clockCont = 0;
+typedef struct mensagem {
+    Clock clock;
+    int destino;
+    int origem;
+} Mensagem;
+
+typedef struct args {
+    int processo;
+    int filaEntradaCont;
+    int filaSaidaCont;
+    Clock clock;
+    pthread_cond_t condFullEntrada;
+    pthread_cond_t condEmptyEntrada;
+    pthread_cond_t condFullSaida;
+    pthread_cond_t condEmptySaida;
+    pthread_mutex_t mutexEntrada;
+    pthread_mutex_t mutexSaida;
+    Clock *filaEntrada;
+    Mensagem *filaSaida;
+} Args;
 
 
-pthread_mutex_t mutex; //mutex para as threads consumidoras e produtoras
-pthread_cond_t condFull;
-pthread_cond_t condEmpty;
-
-int temp_espera_prod; //tempo de espera das threds produtoras
-int temp_espera_cons; //tempo de espera das threads consumidoras
-
+int tempoEspera = 1;
 
 void printClock(Clock *clock, int processo) {
-   printf("Thread: %d, Clock consumido: (%d, %d, %d)\n", processo, clock->p[0], clock->p[1], clock->p[2]);
+   printf("Process: %d, Clock: (%d, %d, %d)\n", processo, clock->p[0], clock->p[1], clock->p[2]);
 }
 
-void produzirClock(int threadId) {
-    //cria o clock
-    Clock *clock = (Clock*)malloc(sizeof(Clock));
-    clock->p[0] = rand() % 10000;
-    clock->p[1] = rand() % 10000;
-    clock->p[2] = rand() % 10000;
+void Event(int pid, Clock *clock){
+   clock->p[pid]++;   
+}
+
+
+void Send(int origem, int destino, Clock *clock){
+   clock->p[origem]++;  //atualiza o clock
+   int * mensagemClock;
+   mensagemClock = calloc (3, sizeof(int));
+   
+   for (int i = 0; i < 3; i++) {
+         mensagemClock[i] = clock->p[i];
+   }
+
+   MPI_Send(mensagemClock, 3, MPI_INT, destino, MPI_ANY_TAG, MPI_COMM_WORLD);
+   
+   free(mensagemClock);
+}
+
+
+Clock* Receive(){
+   int * mensagemClock;
+   mensagemClock = calloc (3, sizeof(int));
+   Clock *clock = (Clock*)malloc(sizeof(Clock));
+   
+   MPI_Recv(mensagemClock, 3,  MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+   
+   for (int i = 0; i < 3; i++) { 
+        clock->p[i] = mensagemClock[i];
+   }
+   
+   free(mensagemClock);
+   return clock;
+}
+
+
+
+
+void* threadRelogio(void* arg) {
+    Args *args = arg;
+    if (args->processo = 0) {
+        Event(0, &args->clock);
     
-    pthread_mutex_lock(&mutex); //bloqueia acesso a região critica
-    //regiao critica
-    while (clockCont == SIZE) { //se tiver cheio espera até receber sinal que um clock foi consumido
-        printf("Thread %d NAO pode produzir pois a fila está cheia\n", threadId);
-        pthread_cond_wait(&condFull, &mutex);
+        
+    }
+    if (args->processo = 1) {
+        
+    }
+    if (args->processo = 2) {
+        
     }
     
-    filaClock[clockCont] = *clock; //insere no final da fila
-    clockCont++;
-    printf("Thread: %d, Clock produzido: (%d, %d, %d)\n", threadId, clock->p[0], clock->p[1], clock->p[2]);
-    
-    pthread_mutex_unlock(&mutex); //desbloqueia acesso a região critica
-    pthread_cond_signal(&condEmpty); //envia sinal que a fila não está vazia
-    
-    free(clock);
+    return NULL;
 }
 
-void consumirClock(int threadId) {
-    pthread_mutex_lock(&mutex); //bloqueia acesso a região critica
-    
-    while (clockCont == 0) { //enquanto a fila estiver vazia espera até que um clock seja produzido
-        printf("Thread %d NAO pode consumir pois a fila esta vazia\n", threadId);
-        pthread_cond_wait(&condEmpty, &mutex);
-    }
-    
-    Clock clock = filaClock[0];
-    printClock(&clock, threadId);
-    for (int i = 0; i < clockCont-1; i++) { //retira o clock do começo da fila
-        filaClock[i] = filaClock[i+1];
-    }
-    clockCont--;
-    
-    pthread_mutex_unlock(&mutex); //desbloqueia acesso a região critica
-    pthread_cond_signal(&condFull); //envia sinal qu a fila não está cheia
-    
-}
-
-void cenarioTesteCheia() {
-    temp_espera_cons = 3;
-    temp_espera_prod = 1;
-}
-
-
-void cenarioTesteVazia() {
-    temp_espera_cons = 1;
-    temp_espera_prod = 3;
-}
-
-
-void *threadProdutora(void* arg) {
-    long id = (long) arg; 
-    while (1){ 
-        produzirClock(id);
-        sleep(temp_espera_prod* (rand() %4));
-   } 
-   return NULL;
-} 
-
-void *threadConsumidora(void* arg) {
-    long id = (long) arg; 
+void* threadSaida(void* arg) {
+    Args *args = arg;
     while(1) {
-        consumirClock(id);  
-        sleep(temp_espera_cons*(rand() %4));
+        pthread_mutex_lock(&(args->mutexSaida));
+        
+        while(args->filaSaidaCont == 0) {
+            pthread_cond_wait(&(args->condEmptySaida), &(args->mutexSaida));
+        }
+        
+        
+        Mensagem mensagem = args->filaSaida[0];
+        //print
+        for(int i = 0; i < (args->filaSaidaCont) -1; i++) {
+            args->filaSaida[i] = args->filaSaida[i+1];
+        }
+        (args->filaSaidaCont)--;
+        
+        pthread_mutex_unlock(&(args->mutexSaida));
+        pthread_cond_signal(&(args->condFullSaida));
     }
     return NULL;
 }
 
-int main() {
-    pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&condEmpty, NULL);
-    pthread_cond_init(&condFull, NULL);
-    
-    pthread_t thread[THREAD_NUM*2]; 
-    
-    cenarioTesteCheia();
-    //cenarioTesteVazia();
-    
-    
-    long i;
-    for (i = 0; i < THREAD_NUM; i++){  
-        if (pthread_create(&thread[i], NULL, &threadConsumidora, (void*) i) != 0) { //cria threads consumidoras
-            perror("Failed to create the thread");
-        }  
+void* threadEntrada(void* arg) {
+    Args *args = arg;
+    while(1) {
+        Clock *clock = Receive();
+        pthread_mutex_lock(&(args->mutexEntrada));
+        
+        while(args->filaEntradaCont == SIZE) {
+            pthread_cond_wait(&(args->condFullEntrada), &(args->mutexEntrada));
+        }
+        
+        args->filaEntrada[args->filaEntradaCont] = *clock;
+        (args->filaEntradaCont)++;
+        
+        pthread_mutex_unlock(&(args->mutexEntrada));
+        pthread_cond_signal(&(args->condEmptyEntrada));
     }
-    for (i = THREAD_NUM; i < THREAD_NUM*2; i++){  
-        if (pthread_create(&thread[i], NULL, &threadProdutora, (void*) i) != 0) { //cria threads produtoras
-            perror("Failed to create the thread");
-        }  
-    }
-    
-    for (i = 0; i < THREAD_NUM*2; i++){  
-        if (pthread_join(thread[i], NULL) != 0) { //join das threads 
-        perror("Failed to join the thread");
-        }  
-    }
-
-    pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&condEmpty);
-    pthread_cond_destroy(&condFull);
-    return 0;
+    return NULL;
 }
+
+
+void processo(int p) {
+    Clock clock = {{0,0,0}};
+    
+    pthread_t tSaida; 
+    pthread_t tEntrada;
+    pthread_t tRelogio;
+    
+    pthread_mutex_t mutexEntrada; //mutex do processo 0
+    pthread_mutex_t mutexSaida;
+    
+    pthread_cond_t condFullEntrada;
+    pthread_cond_t condEmptyEntrada;
+    
+    pthread_cond_t condFullSaida;
+    pthread_cond_t condEmptySaida;
+    
+    int filaEntradaCont = 0;
+    int filaSaidaCont = 0;
+    
+    Clock filaEntrada[SIZE]; //filas do processo
+    Mensagem filaSaida[SIZE];
+    
+    //argumentos para a threadRelogio
+    Args *argsRelogio = (Args*)malloc(sizeof(Args));
+    argsRelogio->clock;
+    argsRelogio->processo = p;
+    argsRelogio-> filaEntradaCont = filaEntradaCont;
+    argsRelogio-> filaSaidaCont = filaSaidaCont;
+    argsRelogio->condEmptyEntrada = condEmptyEntrada;
+    argsRelogio->condFullEntrada = condFullEntrada;
+    argsRelogio->condEmptySaida = condEmptySaida;
+    argsRelogio->condFullSaida = condFullSaida;
+    argsRelogio->mutexEntrada = mutexEntrada;
+    argsRelogio->mutexSaida = mutexSaida;
+    argsRelogio->filaEntrada = filaEntrada;
+    argsRelogio->filaSaida = filaSaida;
+    
+    pthread_cond_init(&(argsRelogio->condFullEntrada), NULL);
+    pthread_cond_init(&(argsRelogio->condEmptyEntrada), NULL);
+    pthread_cond_init(&(argsRelogio->condFullSaida), NULL);
+    pthread_cond_init(&(argsRelogio->condEmptySaida), NULL);
+    pthread_mutex_init(&(argsRelogio->mutexEntrada), NULL);
+    pthread_mutex_init(&(argsRelogio->mutexSaida), NULL);
+    
+
+    //cria threads
+    if (pthread_create(&tRelogio, NULL, &threadRelogio, (void*) argsRelogio) != 0) { //cria thread Relogio
+        perror("Failed to create the thread");
+    }  
+    
+    
+    if (pthread_create(&tEntrada, NULL, &threadEntrada, (void*) argsRelogio) != 0) { //cria thread de entrada
+        perror("Failed to create the thread");
+    }  
+    
+
+    if (pthread_create(&tSaida, NULL, &threadSaida, (void*) argsRelogio) != 0) { //cria thread de saida
+        perror("Failed to create the thread");
+    }  
+    
+    //join das threads 
+    if (pthread_join(tRelogio, NULL) != 0) { //join thread Relogio
+        perror("Failed to join the thread");
+    }  
+
+    if (pthread_join(tEntrada, NULL) != 0) { //join threads entrada
+        perror("Failed to join the thread");
+    }  
+
+
+    if (pthread_join(tSaida, NULL) != 0) { //join threads saida
+        perror("Failed to join the thread");
+    }  
+
+    
+}
+
+
+
+int main(void) {
+   int my_rank;               
+
+   MPI_Init(NULL, NULL); 
+   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
+
+   if (my_rank == 0) { 
+      processo(0);
+   } else if (my_rank == 1) {  
+      processo(1);
+   } else if (my_rank == 2) {  
+      processo(2);
+   }
+
+   /* Finaliza MPI */
+   MPI_Finalize(); 
+
+   return 0;
+}  /* main */
